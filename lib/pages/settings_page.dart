dafea1dd41cc78f9c26c56cf954990f26a19b392
peter-kal <koiliaris.peter@gmail.com>
2026-08-06 18:06:56 +0300

    fix: localizations

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:passie/bloc/blocs.dart';
 import 'package:yaru/yaru.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:passie/l10n/app_localizations.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
